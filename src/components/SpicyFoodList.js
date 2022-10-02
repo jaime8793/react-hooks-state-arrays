e0eb4330a915072f13da7b5765e7d44718ef643d
James Owiti Maurice <james.owiti@student.moringaschool.com>
2022-10-02 23:53:56 +0300

    Completed Assignment

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,24 +3,38 @@ import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  const [filterBy, setFilterBy] = useState("All");
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
+    const newFoods = [...foods, newFood];
+    setFoods(newFoods);
     console.log(newFood);
   }
-
-  const foodList = foods.map((food) => (
-    <li key={food.id}>
-      {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
-    </li>
+  function handleFilterChange(event) {
+    setFilterBy(event.target.value);
+  }
+  const foodsToDisplay = foods.filter((food) => {
+    if (filterBy === "All") return true;
+    return food.cuisine === filterBy;
+  });
+  const foodList = foodsToDisplay.map((food) => (
+    <li key={food.id}>{food.name}</li>
   ));
 
   return (
     <div>
+      <select name="filter" onChange={handleFilterChange}>
+    <option value="All">All</option>
+    <option value="American">American</option>
+    <option value="Sichuan">Sichuan</option>
+    <option value="Thai">Thai</option>
+    <option value="Mexican">Mexican</option>
+  </select>
       <button onClick={handleAddFood}>Add New Food</button>
       <ul>{foodList}</ul>
+
     </div>
   );
 }
-
 export default SpicyFoodList;
